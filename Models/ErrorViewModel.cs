08c5bee07bfb80604a57bfa0a8afca9aff3fdca9
William Wilkes <52538791+Williamcjwilkes@users.noreply.github.com>
2021-11-03 17:43:35 +0000

    added some folders for MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _500084_ACW_2021_Web_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace _500084_ACW_2021_Application
+namespace _500084_ACW_2021_Web_Application
 {
     public class Program
     {
